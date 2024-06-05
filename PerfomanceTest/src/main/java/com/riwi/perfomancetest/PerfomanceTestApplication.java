5062818c4e55469a58b9b6e9accae074949bd2b1
Alejandro Duque <alduk0110@gmail.com>
2024-06-05 06:27:19 -0500

    Project initialize

---
+++ PerfomanceTest/src/main/java/com/riwi/perfomancetest/PerfomanceTestApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.perfomancetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfomanceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfomanceTestApplication.class, args);
+	}
+
+}
